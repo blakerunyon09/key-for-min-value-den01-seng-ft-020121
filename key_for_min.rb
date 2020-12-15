5bd5c2fb8bc20c968ca4c5ea316b6c78e835ae59
Blake Runyon <blakerunyon09@gmail.com>
2020-12-15 20:11:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.each do
 end
+
+hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+key_for_min_value(hash)
